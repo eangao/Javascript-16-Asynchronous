fe6a8ecdcaf59b02387d0546b3614aa866169f9b
eangao <elmarcera@gmail.com>
2023-12-26 16:11:15 +0800

    Promises and the Fetch API

--- script.js
+++ script.js
@@ -131,114 +131,142 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  // request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   // request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 
-  //use this api if the above is updated or change
-  // like from previous training it uses v.2 and now v.3.1
-  request.open(
-    'GET',
-    `https://countries-api-836d.onrender.com/countries/name/${country}`
-  );
-  request.send();
+//   //use this api if the above is updated or change
+//   // like from previous training it uses v.2 and now v.3.1
+//   request.open(
+//     'GET',
+//     `https://countries-api-836d.onrender.com/countries/name/${country}`
+//   );
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    // Render country 1
-    renderCountry(data);
+//     // Render country 1
+//     renderCountry(data);
 
-    // Get neighbour country (2)
-    const [neighbour] = data.borders;
+//     // Get neighbour country (2)
+//     const [neighbour] = data.borders;
 
-    if (!neighbour) return;
+//     if (!neighbour) return;
 
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open(
-      'GET',
-      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
-    );
-    request2.send();
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open(
+//       'GET',
+//       `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+//     );
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log(data2);
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
 
-      renderCountry(data2, 'neighbour');
-    });
-  });
-};
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
 
-// getCountryAndNeighbour('portugal');
-getCountryAndNeighbour('usa');
+// // getCountryAndNeighbour('portugal');
+// getCountryAndNeighbour('usa');
 
-// So in other words, here, we have nested callbacks.
-// But now imagine that we wanted
-// to do more requests in sequence,
-// like the neighbor of the neighbor of the neighbor,
-// and like 10 times over.
+// // So in other words, here, we have nested callbacks.
+// // But now imagine that we wanted
+// // to do more requests in sequence,
+// // like the neighbor of the neighbor of the neighbor,
+// // and like 10 times over.
 
-// So in that case, we would end up
-// with callbacks inside of callbacks inside of callbacks,
-// like 10 times.
-// And actually, for that kind of structure.
-// And for that kind of behavior, we have a special name.
-// And that special name is callback hell.
+// // So in that case, we would end up
+// // with callbacks inside of callbacks inside of callbacks,
+// // like 10 times.
+// // And actually, for that kind of structure.
+// // And for that kind of behavior, we have a special name.
+// // And that special name is callback hell.
 
-// So basically, callback hell
-// is when we have a lot of nested callbacks
-// in order to execute asynchronous tasks in sequence.
-// And in fact, this happens for all asynchronous tasks,
-// which are handled by callbacks.
-// And not just AJAX calls.
+// // So basically, callback hell
+// // is when we have a lot of nested callbacks
+// // in order to execute asynchronous tasks in sequence.
+// // And in fact, this happens for all asynchronous tasks,
+// // which are handled by callbacks.
+// // And not just AJAX calls.
 
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 second passed');
-    setTimeout(() => {
-      console.log('3 second passed');
-      setTimeout(() => {
-        console.log('4 second passed');
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 second passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
 
-// we have callback hell.
-// And in fact, callback hell is pretty easy to identify
-// by this triangular shape that is formed here, you see that.
-// And also the same is starting to appear here.
-// So if we had more callbacks in here,
-// then we would start to see a lot more indentation here.
-// And then this triangular shape, would also appear there.
-// Now, the problem with callback hell
-// is that it makes our code look very messy.
-// But even more important,
-// it makes our code harder to maintain,
-// and very difficult to understand, and to reason about,
-// and code that is hard to understand
-// and difficult to reason about.
-// Will have more bugs, and it's just worse code.
-// So this is a great rule
+// // we have callback hell.
+// // And in fact, callback hell is pretty easy to identify
+// // by this triangular shape that is formed here, you see that.
+// // And also the same is starting to appear here.
+// // So if we had more callbacks in here,
+// // then we would start to see a lot more indentation here.
+// // And then this triangular shape, would also appear there.
+// // Now, the problem with callback hell
+// // is that it makes our code look very messy.
+// // But even more important,
+// // it makes our code harder to maintain,
+// // and very difficult to understand, and to reason about,
+// // and code that is hard to understand
+// // and difficult to reason about.
+// // Will have more bugs, and it's just worse code.
+// // So this is a great rule
 
-// that you should always remember and keep in mind.
-// So again, the rule is that code that's hard to understand,
-// is basically bad code, because it will have more bugs,
-// because the harder it is to understand code
-// and to reason about the code,
-// the more difficult it will be to add new features
-// and to add more functionality to the application.
+// // that you should always remember and keep in mind.
+// // So again, the rule is that code that's hard to understand,
+// // is basically bad code, because it will have more bugs,
+// // because the harder it is to understand code
+// // and to reason about the code,
+// // the more difficult it will be to add new features
+// // and to add more functionality to the application.
 
-// But anyway, given all these problems with callback hell,
-// we of course, need a way to solve callback hell.
-// And fortunately for us, since ES6,
-// there is actually a way of escaping callback hell
-// by using something called promises.
-// And so let's now take the next step
-// in our journey of asynchronous JavaScript,
-// which is to learn all about promises.
+// // But anyway, given all these problems with callback hell,
+// // we of course, need a way to solve callback hell.
+// // And fortunately for us, since ES6,
+// // there is actually a way of escaping callback hell
+// // by using something called promises.
+// // And so let's now take the next step
+// // in our journey of asynchronous JavaScript,
+// // which is to learn all about promises.
+
+//////////////////////////////////////////////////////////////////
+// Promises and the Fetch API
+//////////////////////////////////////////////////////////////////
+
+//SEE PDF LECTURE AND VIDEO
+
+// const request = new XMLHttpRequest();
+// request.open(
+//   'GET',
+//   `https://countries-api-836d.onrender.com/countries/name/${country}`
+// );
+// request.send();
+
+// Now there are actually some more options
+// that we can specify here
+// in the fetch function, if we'd like,
+// but to make a simple get request like this one,
+// all we really need is to pass in the URL
+// and that's it.
+// So for more complex AJAX calls,
+// the fetch function can take in like an object
+// of options as well
+// But again, for now, we don't need that.
+const request = fetch(
+  'https://countries-api-836d.onrender.com/countries/name/portugal'
+);
+console.log(request);
